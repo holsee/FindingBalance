b917e8ab3696e3ea5a43dbc628bcec89a14f3a83
holsee <sholdsworth@gmail.com>
2017-08-09 21:29:11 +0100

    yarp

--- haskell/test.hs
+++ haskell/test.hs
@@ -1,10 +1,11 @@
-x = [1..100000] ++ [100000,99999..1]
+x :: [Int]
+x = [1..10000] ++ [10000,9999..1]
 
 go [] = Left "Nope"
 go list = test list 0
 
 goEfficient [] = Left "Nope"
-goEfficient (x:xs) = efficientTest [] (x:xs) (0, sum (x:xs)) 0
+goEfficient (x:xs) = efficientTest (x:xs) (0, sum (x:xs)) 0
 
 test :: [Int] -> Int -> Either String Int
 test list index | index < length list = case leftSum == rightSum of
@@ -14,7 +15,8 @@ test list index | index < length list = case leftSum == rightSum of
           where leftSum = sum (drop index list)
                 rightSum = sum (take index list)
 
-efficientTest :: (Num a, Enum a, Eq a) => [a] -> [a] -> (a, a) -> a -> Either String a
-efficientTest x [] _ acc = Left "Nope"
-efficientTest xs (y:ys) (sumX, sumY) index | sumX + y == sumY - y = Right index
-                                           | otherwise = efficientTest (y:xs) ys (sumX + y, sumY - y) (succ index)
+efficientTest :: (Num a, Enum a, Eq a) => [a] -> (a, a) -> a -> Either String a
+efficientTest [] _ acc = Left "Nope"
+efficientTest (y:ys) (sumX, sumY) index 
+  | sumX + y == sumY - y = Right index
+  | otherwise = efficientTest ys (sumX + y, sumY - y) (succ index)
